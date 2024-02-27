72e73d5ef7781fc451413efe9b179c673b434193
jae6269 <jae6269@gmail.com>
2024-02-27 17:51:50 +0900

    fix : Card 컴포넌트 생성

---
+++ src/components/common/Card/Card.jsx
@@ -0,0 +1,14 @@
+import './card.scss';
+
+function Card() {
+  const CARD_DATA_URL =
+    'https://rolling-api.vercel.app/4-20//recipients/1/messages/';
+  const testId = 1;
+  return (
+    <div>
+      <button></button>
+    </div>
+  );
+}
+
+export default Card;
