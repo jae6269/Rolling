be7968a54ad45cb48a9cf628728b5def877d3de6
jae6269 <jae6269@gmail.com>
2024-02-27 16:30:55 +0900

    feat : useFetchData hook 추가

---
+++ src/hooks/useFetchData.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+/**
+ *
+ */
+function useFetchData(url) {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(url);
+        const result = await response.json();
+        setData([...result.data]);
+      } catch (error) {
+        console.log(`${url}에 대한 fetch error : ${error}`);
+      }
+    };
+    getData();
+  }, []);
+
+  return data;
+}
+
+export default useFetchData;
