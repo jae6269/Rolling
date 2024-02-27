ae8f8951cee061d1db0f37e317666e9e5d5af1d4
HaeJungg <gpwnd0120@gmail.com>
2024-02-27 18:00:45 +0900

    fix : Eslint 오류 수정

--- src/components/Option/index.jsx
+++ src/components/Option/index.jsx
@@ -1,21 +1,38 @@
 import { useState } from 'react';
 import { optionBtn } from '../../utils/imageImport';
+import './index.module.scss';
 
 const Option = () => {
-  const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedColor, setSelectedColor] = useState(null);
+
+  const handleColorSelect = color => {
+    setSelectedColor(color);
+  };
 
   return (
     <div className="styles.color-option">
-      <div className="styles.beige">
+      <div
+        className={`${color - palette} ${styles['beige']}`}
+        onClick={() => handleColorSelect('beige')}
+      >
         <img src={optionBtn} alt="option button" />
       </div>
-      <div className="styles.purple">
+      <div
+        className={`${color - palette} ${styles['beige']}`}
+        onClick={() => handleColorSelect('purple')}
+      >
         <img src={optionBtn} alt="option button" />
       </div>
-      <div className="styles.blue">
+      <div
+        className={`${color - palette} ${styles['beige']}`}
+        onClick={() => handleColorSelect('blue')}
+      >
         <img src={optionBtn} alt="option button" />
       </div>
-      <div className="styles.green">
+      <div
+        className={`${color - palette} ${styles['beige']}`}
+        onClick={() => handleColorSelect('green')}
+      >
         <img src={optionBtn} alt="option button" />
       </div>
     </div>
