c3194ef5e551520ca72b6407a3b7ab8b1251a94e
HaeJungg <gpwnd0120@gmail.com>
2024-02-27 17:33:17 +0900

    feat : 옵션 이미지 선택 div 생성

---
+++ src/components/Option/index.jsx
@@ -0,0 +1,12 @@
+const Option = () => {
+  return (
+    <div className="styles.color-option">
+      <div className="styles.beige"></div>
+      <div className="styles.purple"></div>
+      <div className="styles.blue"></div>
+      <div className="styles.green"></div>
+    </div>
+  );
+};
+
+export default Option;
