5369ded75f526f356b96d1eb13a9076d333e0de4
HaeJungg <gpwnd0120@gmail.com>
2024-02-27 17:46:01 +0900

    feat : 이미지 선택에 따라서 바뀌는 useState 생성

--- src/components/Option/index.jsx
+++ src/components/Option/index.jsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
+import { optionBtn } from '../../utils/imageImport';
+
 const Option = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
   return (
     <div className="styles.color-option">
-      <div className="styles.beige"></div>
-      <div className="styles.purple"></div>
-      <div className="styles.blue"></div>
-      <div className="styles.green"></div>
+      <div className="styles.beige">
+        <img src={optionBtn} alt="option button" />
+      </div>
+      <div className="styles.purple">
+        <img src={optionBtn} alt="option button" />
+      </div>
+      <div className="styles.blue">
+        <img src={optionBtn} alt="option button" />
+      </div>
+      <div className="styles.green">
+        <img src={optionBtn} alt="option button" />
+      </div>
     </div>
   );
 };
