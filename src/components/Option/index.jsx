2e6503e66fc53747b920413ef6acc4e93df423ac
HaeJungg <gpwnd0120@gmail.com>
2024-02-28 17:26:56 +0900

    style : 토글버튼, 버튼 scss 추가

--- src/components/Option/index.jsx
+++ src/components/Option/index.jsx
@@ -6,18 +6,17 @@ import optionBtn from '../../utils/imageImport';
 const Option = () => {
   const [selectedOption, setSelectedOption] = useState('color');
   const [selectedButton, setSelectedButton] = useState(null);
-  const colorValues = ['beige', 'purple', 'blue', 'green']; // 컬러 값 배열
+  const colorValues = ['beige', 'purple', 'blue', 'green'];
 
-  // API 응답 전체를 받습니다.
+  // url 공통 상수 파일 만들면 수정
   const apiResponse = useFetchData(
     'https://rolling-api.vercel.app/background-images/',
   );
-  // 응답에서 imageUrls 배열을 안전하게 추출합니다.
   const imageUrls = apiResponse ? apiResponse.imageUrls : [];
 
   const handleToggle = option => {
     setSelectedOption(option);
-    setSelectedButton(null); // 토글 변경 시 선택된 버튼 초기화
+    setSelectedButton(null);
   };
 
   const handleButtonClick = buttonIndex => {
@@ -36,26 +35,25 @@ const Option = () => {
       <div className={styles.toggleButtons}>
         <button
           type="button"
-          className={selectedOption === 'image' ? styles.selectedOption : ''}
-          onClick={() => handleToggle('image')}
-        >
-          이미지
-        </button>
-        <button
-          type="button"
           className={selectedOption === 'color' ? styles.selectedOption : ''}
           onClick={() => handleToggle('color')}
         >
           컬러
         </button>
+        <button
+          type="button"
+          className={selectedOption === 'image' ? styles.selectedOption : ''}
+          onClick={() => handleToggle('image')}
+        >
+          이미지
+        </button>
       </div>
-
       <div className={styles.buttonsContainer}>
         {[0, 1, 2, 3].map(index => (
           <button
             key={index}
             type="button"
-            className={styles.buttonStyle}
+            className={`${styles.buttonStyle} ${selectedOption === 'color' ? styles[colorValues[index]] : ''}`}
             style={{
               backgroundImage:
                 selectedOption === 'image' && imageUrls[index]
