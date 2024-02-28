ae4a8ddfb127b5300fe6e920368c6fe2852fd002
jae6269 <jae6269@gmail.com>
2024-02-28 20:42:02 +0900

    docs : Modal 폴더, 파일 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import Modal from './components/Modal/Modal';
 
 function App() {
-  return <div>app</div>;
+  return <Modal />;
 }
 
 export default App;
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './modal.scss';
+
+function Modal(props) {
+  return (
+    <div className="modal-background">
+      <div className="modal-container">모달 컨테이너</div>
+    </div>
+  );
+}
+export default Modal;
