e318e74a562d0f818842089071fa01aa1d72a20d
HaeJungg <gpwnd0120@gmail.com>
2024-02-28 17:29:20 +0900

    fix : app.js 이전으로 되돌리기

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React from 'react';
 import Option from './components/Option';
 
 function App() {
-  return (
-    <div>
-      <Option />
-    </div>
-  );
+  return <div>app</div>;
 }
 
 export default App;
