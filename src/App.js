a852faa22e1ee942906e7376ae63ce2695e54eaf
leehj67 <109390172+leehj67@users.noreply.github.com>
2024-02-28 15:44:28 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import Friend from './BadgeTagFriends';
-import Badge from './BadgeComponent';
-import Family from './BadgeTagFamily';
-import Just from './BadgeTagJustKnow';
-import Col from './BadgeTagCollege'
 
 function App() {
-  return <div><Friend/> <Badge/> <Family/><Just/><Col /></div>;
+  return <div>app</div>;
 }
 
 export default App;
