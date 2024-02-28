0b6e3c447c8126a6d4b082b25c55cb6ca19754fd
HaeJungg <gpwnd0120@gmail.com>
2024-02-28 16:40:12 +0900

    fix :fetch 데이터 result 전체를 반환하도록 변경

--- src/utils/formatDataFunctions.js
+++ src/utils/formatDataFunctions.js
@@ -1,5 +1,5 @@
 // useFetchData에서 받아온 데이터를
 // 컴포넌트에 맞게 포매팅 해주는 함수입니다
 
-//함수명 : formatXData
-//예시 : formatCardData, formatProfileData
+// 함수명 : formatXData
+// 예시 : formatCardData, formatProfileData
