d0622b65391d43b37f2056cd56747fe117201274
leehj67 <109390172+leehj67@users.noreply.github.com>
2024-02-28 20:17:29 +0900

    폴더 수정
    폴더 수정, 구조 변경

---
+++ components/EmogeBadge.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// SCSS 모듈을 스타일 객체로 임포트
+import styles from './BadgeDesign.module.scss';
+
+function EmogeBadge() {
+  return (
+    <div className={styles.badge}>
+      <div className={styles.badge_emoji}>
+        <div className={styles.ic}>😍</div>
+        <div className={styles.img_text}>24</div>
+      </div>
+    </div>
+  );
+}
+
+export default EmogeBadge;
