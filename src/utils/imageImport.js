be68f6aa01e193def72254e312fd83dda5e2ccb6
HaeJungg <gpwnd0120@gmail.com>
2024-03-01 21:41:33 +0900

    feat: 이미지 import 추가

--- src/utils/imageImport.js
+++ src/utils/imageImport.js
@@ -5,4 +5,22 @@ import blueShadow from '../assets/blue-shadow.svg';
 import greenShadow from '../assets/green-shadow.svg';
 import purpleShadow from '../assets/purple-shadow.svg';
 
-export { optionBtn, beigeShadow, blueShadow, greenShadow, purpleShadow };
+import addEmojiBtn from '../assets/add-emoji.svg';
+import arrowDownBtn from '../assets/arrow_down.svg';
+import shareBtn from '../assets/share-button.svg';
+import verticalLine from '../assets/vertical-line.svg';
+
+import logoImg from '../assets/logo-img.svg';
+
+export {
+  optionBtn,
+  beigeShadow,
+  blueShadow,
+  greenShadow,
+  purpleShadow,
+  addEmojiBtn,
+  arrowDownBtn,
+  shareBtn,
+  verticalLine,
+  logoImg,
+};
