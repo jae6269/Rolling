5edc9502f545a46859b2c49718270c041c4b8cda
Songeun <hannahkeeien@gmail.com>
2024-03-01 15:38:09 +0900

    feat: header 컴포넌트 구현

--- src/components/common/Header/index.jsx
+++ src/components/common/Header/index.jsx
@@ -1,7 +1,7 @@
 import logoImg from "../../../assets/logo-img.svg";
 import styles from "./index.module.scss";
 
-//<Header buttonOn={true} /> 이런식으로 사용
+//<Header buttonOn={true} /> 이런식으로 사용한다.
 function Header({ buttonOn }) {
   return (
     <div className={styles.header}>
