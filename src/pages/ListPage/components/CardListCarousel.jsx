9fc207173839782df14873d1528c271fafe47fc8
HaeJungg <gpwnd0120@gmail.com>
2024-03-02 16:00:23 +0900

    feat : 캐러셀에 연결

--- src/pages/ListPage/components/CardListCarousel.jsx
+++ src/pages/ListPage/components/CardListCarousel.jsx
@@ -1,29 +1,29 @@
 // card carousel
-
 import React from 'react';
 import Slider from 'react-slick';
 import ListOfCard from '../../../components/CardList/ListCard';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import { NextArrow, PrevArrow } from './Arrow';
 
 const CardCarousel = ({ cards }) => {
   const settings = {
-    dots: true,
-    infinite: false,
+    dots: false, // 아래 점들을 표시할지 여부
+    infinite: false, // 무한 스크롤 여부
     speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-    // 화살표 컴포넌트 추가
-    // nextArrow: <YourNextArrow />,
-    // prevArrow: <YourPrevArrow />,
+    slidesToShow: 1, // 한 번에 보여줄 슬라이드 수
+    slidesToScroll: 1, // 한 번에 넘길 슬라이드 수
+    variableWidth: true, // 다양한 너비의 슬라이드를 지원
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
     responsive: [
       {
         breakpoint: 768, // 태블릿과 모바일에 대한 설정
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          swipeToSlide: true,
+          swipeToSlide: true, // 스와이프 기능 활성화
+          variableWidth: false, // 슬라이드 너비 고정
         },
       },
     ],
