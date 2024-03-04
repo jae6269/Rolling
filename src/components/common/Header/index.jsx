57b4f6b266746b3ccf9deb584895f16637593a92
Songeun <hannahkeeien@gmail.com>
2024-03-04 14:36:11 +0900

    feat: header 로고 클릭시 메인페이지로 이동

--- src/components/common/Header/index.jsx
+++ src/components/common/Header/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
-import logoImg from '../../../utils/imageImport';
+import { Link } from 'react-router-dom';
+import { logoImg } from '../../../utils/imageImport';
 import styles from './index.module.scss';
 
 // <Header buttonOn={false} /> 이런식으로 사용
@@ -8,7 +8,9 @@ function Header({ buttonOn = true }) {
   return (
     <div className={styles.header}>
       <div className={styles.headerinside}>
-        <img className={styles.logo} src={logoImg} alt="로고 이미지" />
+        <Link to="/">
+          <img className={styles.logo} src={logoImg} alt="로고 이미지" />
+        </Link>
         {/* 버튼 컴포넌트 scss: 상민님 */}
         {/* 나중에 페이지 만들었을 때 아래 코드 사용 */}
         {/* 지금은 페이지 구성을 안해서 그런지 Link적용이 안됨 */}
