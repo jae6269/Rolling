28e9a1d9256fbbb5e22f3638e8d15e42702adf69
jae6269 <jae6269@gmail.com>
2024-03-07 23:21:53 +0900

    fix: 로딩시간 1초로 변경

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Route, Routes, BrowserRouter, useLocation } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import MainPage from './pages/MainPage/MainPage';
 import ListPage from './pages/ListPage/ListPage';
 import CardCreatePage from './pages/CardCreatePage/CardCreatePage';
@@ -16,7 +16,7 @@ function App() {
       setIsLoading(true);
       setTimeout(() => {
         setIsLoading(false);
-      }, 2000); // 예시로 1초 뒤에 로딩 상태를 false로 변경
+      }, 1000); // 예시로 1초 뒤에 로딩 상태를 false로 변경
     };
 
     handleLoading(); // 라우트 변경 시 핸들러 실행
