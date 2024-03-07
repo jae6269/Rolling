8e20fbc9f6cbed2751015c8fb76de4bc7a881b38
jae6269 <jae6269@gmail.com>
2024-03-07 15:23:24 +0900

    feat: 로딩화면구현, App.js에 연결

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, createContext } from 'react';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import MainPage from './pages/MainPage/MainPage';
 import ListPage from './pages/ListPage/ListPage';
@@ -6,19 +6,26 @@ import CardCreatePage from './pages/CardCreatePage/CardCreatePage';
 import PostPage from './pages/PostPage/PostPage';
 import PostEditPage from './pages/PostEditPage/PostEditPage';
 import MessageCreatePage from './pages/MessageCreatePage/MessageCreatePage';
+import Loading from './components/common/Loading/Loading';
+
+export const LoadingContext = createContext(null);
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/post" element={<CardCreatePage />} />
-        <Route path="/post/:id" element={<PostPage />} />
-        <Route path="/post/:id/edit" element={<PostEditPage />} />
-        <Route path="/post/:id/message" element={<MessageCreatePage />} />
-        <Route path="/list" element={<ListPage />} />
-      </Routes>
-    </BrowserRouter>
+    <LoadingContext.Provider value={(isLoading, setIsLoading)}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/post" element={<CardCreatePage />} />
+          <Route path="/post/:id" element={<PostPage />} />
+          <Route path="/post/:id/edit" element={<PostEditPage />} />
+          <Route path="/post/:id/message" element={<MessageCreatePage />} />
+          <Route path="/list" element={<ListPage />} />
+        </Routes>
+      </BrowserRouter>
+      {isLoading && <Loading />}
+    </LoadingContext.Provider>
   );
 }
 
