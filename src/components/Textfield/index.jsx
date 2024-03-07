5a909f35071ae67056530d749de46fd2fdad264e
HaeJungg <gpwnd0120@gmail.com>
2024-03-07 18:32:42 +0900

    feat: dropdown 이미지 추가

--- src/components/Textfield/index.jsx
+++ src/components/Textfield/index.jsx
@@ -1,27 +1,41 @@
 import React, { useState } from 'react';
 import { arrowTopBtn, arrowDownBtn } from '../../utils/imageImport';
 
-function Dropdown({ label, options, onChange, value }) {
-  const [error, setError] = useState('');
+function Dropdown({ options }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(
+    options[0] || 'Placeholder',
+  );
 
-  const handleChange = event => {
-    const newValue = event.target.value;
-    setError(newValue ? '' : 'Please select an option');
-    onChange(newValue);
+  const handleOptionClick = option => {
+    setSelectedOption(option);
+    setIsOpen(false); // 드롭다운을 닫습니다.
   };
 
   return (
     <div className="dropdown">
-      {label && <label htmlFor="dropdown">{label}</label>}
-      <select id="dropdown" onChange={handleChange} value={value}>
-        <option value="">Select...</option>
-        {options.map(option => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </select>
-      {error && <span className="error">{error}</span>}
+      <div className="dropdown-header">
+        {selectedOption}
+        <img
+          src={isOpen ? arrowTopBtn : arrowDownBtn}
+          className="dropdown-icon"
+          alt="Toggle Dropdown"
+          onClick={() => setIsOpen(!isOpen)}
+        />
+      </div>
+      {isOpen && (
+        <ul className="dropdown-list">
+          {options.map((option, index) => (
+            <li
+              key={index}
+              className="dropdown-item"
+              onClick={() => handleOptionClick(option)} // 여기서 옵션을 선택하고 드롭다운을 토글합니다.
+            >
+              {option}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
