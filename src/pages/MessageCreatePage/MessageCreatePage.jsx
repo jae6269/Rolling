90b180d966e6f67d6b865875f25e9c8e6cf14b9b
HaeJungg <gpwnd0120@gmail.com>
2024-03-07 20:59:28 +0900

    chore: 페이지 파일 되돌리기

--- src/pages/MessageCreatePage/MessageCreatePage.jsx
+++ src/pages/MessageCreatePage/MessageCreatePage.jsx
@@ -1,15 +1,6 @@
 import React from 'react';
-import Dropdown from '../../components/Textfield';
 
 function MessageCreatePage() {
-  const options = ['Apple', 'Banana', 'Orange', 'Grape'];
-
-  return (
-    <div className="App">
-      <h1>Dropdown 예시</h1>
-      <Dropdown options={options} />
-      <h1>Dropdown 예시</h1>
-    </div>
-  );
+  return <div>MessageCreatePage</div>;
 }
 export default MessageCreatePage;
