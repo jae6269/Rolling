e7ceabd64b1c11ff3ed5286a2ce72ce6c3195164
jae6269 <jae6269@gmail.com>
2024-03-07 17:27:07 +0900

    style: Loading 띄어쓰기 변경

--- src/components/common/Loading/Loading.jsx
+++ src/components/common/Loading/Loading.jsx
@@ -5,7 +5,7 @@ import Spinner from '../../../assets/loadingSpinner.gif';
 function Loading() {
   return (
     <div className={styles.loadingBackground}>
-      <p className={styles.loadingText}>로딩중입니다!</p>
+      <p className={styles.loadingText}>로딩중입니다 !</p>
       <img className={styles.loadingSpinner} src={Spinner} alt="spinner" />
     </div>
   );
