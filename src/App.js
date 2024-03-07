c7176a35cddf6cb26a5c85762f62efac44869897
jae6269 <jae6269@gmail.com>
2024-03-07 17:38:54 +0900

    fix: Toast 컴포넌트 가운데정렬

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import MainPage from './pages/MainPage/MainPage';
 import ListPage from './pages/ListPage/ListPage';
 import CardCreatePage from './pages/CardCreatePage/CardCreatePage';
 import PostPage from './pages/PostPage/PostPage';
-import PostEditPage from './pages/PostEditPage/PostEditPage';
 import MessageCreatePage from './pages/MessageCreatePage/MessageCreatePage';
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/post" element={<CardCreatePage />} />
         <Route path="/post/:id" element={<PostPage />} />
-        <Route path="/post/:id/edit" element={<PostEditPage />} />
         <Route path="/post/:id/message" element={<MessageCreatePage />} />
         <Route path="/list" element={<ListPage />} />
       </Routes>
