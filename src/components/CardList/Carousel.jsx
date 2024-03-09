1ad40ae288583dd53f2a6f428c367551d2fa9e99
HaeJungg <gpwnd0120@gmail.com>
2024-03-09 15:29:53 +0900

    feat: 스켈레톤 UI 추가, 로딩 이벤트 처리

--- src/components/CardList/Carousel.jsx
+++ src/components/CardList/Carousel.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect, useRef } from 'react';
 import ListOfCard from './ListOfCard';
 import styles from './carousel.module.scss';
+import CardSkeleton from './CardSkeleton';
 
 const Carousel = ({ cards }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isLeftButtonVisible, setIsLeftButtonVisible] = useState(false);
   const [isRightButtonVisible, setIsRightButtonVisible] = useState(true);
   const carouselRef = useRef(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   const { current: carouselElement } = carouselRef;
 
@@ -42,11 +44,15 @@ const Carousel = ({ cards }) => {
     }
   };
 
-  // 카드 수 계산
+  // 카드 수 계산 (로딩 완료 후에만 실행)
   const calculateVisibleCards = () => {
-    const wrapperWidth = carouselElement.clientWidth;
-    const cardWidthWithGap = cardWidth + gap;
-    return Math.floor(wrapperWidth / cardWidthWithGap);
+    if (carouselElement) {
+      // 캐러셀 요소가 존재하는 경우에만 계산
+      const wrapperWidth = carouselElement.clientWidth;
+      const cardWidthWithGap = cardWidth + gap;
+      return Math.floor(wrapperWidth / cardWidthWithGap);
+    }
+    return 0; // 로딩 중이거나 캐러셀 요소가 없는 경우 0 반환
   };
 
   // 버튼누르면 보이는 카드 갯수만큼 넘어가도록 수정
@@ -66,6 +72,15 @@ const Carousel = ({ cards }) => {
     slideTo(newIndex);
   };
 
+  // 데이터 로딩 및 스켈레톤 처리 이펙트
+  useEffect(() => {
+    const simulateLoading = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(simulateLoading);
+  }, []);
+
   return (
     <div className={styles.carouselContainer}>
       {isLeftButtonVisible && (
@@ -82,7 +97,22 @@ const Carousel = ({ cards }) => {
         ref={carouselRef}
         onScroll={handleScroll}
       >
-        {cards.length === 0 ? (
+        {/* eslint-disable-next-line no-nested-ternary */}
+        {isLoading ? (
+          Array(6)
+            .fill(null)
+            .map((_, index) => (
+              <div
+                // eslint-disable-next-line react/no-array-index-key
+                key={`skeleton-${index}`}
+                style={{
+                  marginRight: index !== cards.length - 1 ? `${gap}px` : 0,
+                }}
+              >
+                <CardSkeleton />
+              </div>
+            ))
+        ) : cards.length === 0 ? (
           <p>No cards to display.</p>
         ) : (
           cards.map((card, index) => (
