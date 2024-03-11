847d9cfc8361346d7654260d7133c2aa27d63229
HaeJungg <gpwnd0120@gmail.com>
2024-03-11 11:57:15 +0900

    feat: 삭제가 완료되야 리스트로 넘어가도록 수정

--- src/components/Modal/MessageClearModal.jsx
+++ src/components/Modal/MessageClearModal.jsx
@@ -4,13 +4,18 @@ import styles from './messageClearModal.module.scss';
 
 function MessageClearModal({ url, handleClose }) {
   const navigate = useNavigate();
-  const handleMessageClear = e => {
+  const handleMessageClear = async e => {
     e.preventDefault();
     handleClose(e);
-    fetch(url, {
-      method: 'DELETE',
-    });
-    navigate('/list');
+    try {
+      await fetch(url, {
+        method: 'DELETE',
+      });
+      // 패치 요청이 완료되면 리스트로 이동
+      navigate('/list');
+    } catch (error) {
+      console.error('롤링 페이퍼 삭제 중 오류가 발생했습니다:', error);
+    }
   };
   return (
     <div className={styles.clearModalBackground}>
