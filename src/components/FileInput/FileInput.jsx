9af4a1d2612ed9f1a91ee3e4c01fd981c303d40c
Songeun <hannahkeeien@gmail.com>
2024-03-11 10:14:18 +0900

    chore: 주석 추가

--- src/components/FileInput/FileInput.jsx
+++ src/components/FileInput/FileInput.jsx
@@ -3,6 +3,8 @@ import styles from './fileInput.module.scss';
 import { plus } from '../../utils/imageImport';
 
 function FileInput({ setSelectedImage, fileInputRef, setProfileImage }) {
+  // 파일이 업로드 되면 실행할 함수
+  // 이미지 미리보기, 이미지 업로드 사이트에 이미지 업로드 하고 url받아와서 롤링 서버로 보내기
   const handleFileInputChange = async e => {
     const fileUrl = URL.createObjectURL(e.target.files[0]);
     setSelectedImage(fileUrl);
@@ -29,6 +31,7 @@ function FileInput({ setSelectedImage, fileInputRef, setProfileImage }) {
   };
   return (
     <>
+      {/* label을 클릭하면 파일 업로드가 되게끔 하고 진짜 파일 인풋 부분은 숨김처리 */}
       <label className={styles.inputFileButton} htmlFor="input-file">
         <img className={styles.inputFileButtonImage} src={plus} alt="addFile" />
       </label>
